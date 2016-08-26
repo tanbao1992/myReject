cc8a5b05d1ba5bdc3bfa29b8f6414db8bfeef348
tan <1849805669@qq.com>
2016-08-26 20:25:02 +0800

    豹的第一次提交小项目

---
+++ 轮播/css.js
@@ -0,0 +1,32 @@
+*{
+				margin: 0;
+				padding: 0;
+				box-sizing: border-box;
+			}
+			html{
+				height: 100%;
+			}
+			body{
+				height: 100%;
+				margin: 0;
+				background-color: black;
+				
+			}
+			.container{
+				width:600px;
+				height: 450px;
+				margin: 100px auto;
+				position: relative;
+				
+				
+			}
+			img{
+				position: absolute;
+				border: 10px solid white ;
+				box-shadow: 0  0  10px gold;
+			}
+			#dome{
+				position: relative;
+				width:200px;
+				height: 100px;
+			}
